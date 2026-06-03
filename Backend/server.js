82eb2ca0089a587ecd3a451e83c4c51c9abd1000
fatofato24 <fatimamushtaq2412@gmail.com>
2026-06-03 23:04:48 +0500

    Database added and user made for the database

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
